4c8185f903b8a5721bb473c838f6d1407e1ae720
Shui H. Kwok <shkw0k@users.noreply.github.com>
2019-10-16 11:27:14 -1000

    Do not use memmap to reduce number of open files

--- keckdrpframework/primitives/simple_fits_reader.py
+++ keckdrpframework/primitives/simple_fits_reader.py
@@ -1,8 +1,10 @@
-'''
+"""
 Created on Jul 9, 2019
+
+Be aware that hdus.close () needs to be called to limit the number of open files at a given time.
                 
 @author: skwok
-'''
+"""
 
 import astropy.io.fits as pf 
 from astropy.utils.exceptions import AstropyWarning
@@ -13,25 +15,25 @@ from keckdrpframework.primitives.base_primitive import Base_primitive
 
 def open_nowarning (filename):
     with warnings.catch_warnings():
-        warnings.simplefilter('ignore', AstropyWarning)
-        return pf.open(filename)
+        warnings.simplefilter("ignore", AstropyWarning)
+        return pf.open(filename, memmap=False)
 
 class simple_fits_reader (Base_primitive):
-    '''
+    """
     classdocs
-    '''
+    """
 
     def __init__(self, action, context):
-        '''
+        """
         Constructor
-        '''    
+        """    
         Base_primitive.__init__(self, action, context)
         
     def _perform (self):
-        '''
+        """
         Expects action.args.name as fits file name
         Returns HDUs or (later) data model
-        '''
+        """
         name = self.action.args.name
         self.logger.info (f"Reading {name}")
         out_args = Arguments()
