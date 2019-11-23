447589ef2c322b02186109c19828f8b08770b616
Shui H. Kwok <shkw0k@users.noreply.github.com>
2019-11-22 17:35:47 -1000

    Test to run the example pipeline

---
+++ keckdrpframework/unit_tests/test_run_example.py
@@ -0,0 +1,25 @@
+#
+# Test running the example 
+#
+# Created: 2019-11-22, skwok
+#
+import pytest
+import sys
+import glob
+from keckdrpframework.config.framework_config import ConfigClass
+from keckdrpframework.core.framework import Framework
+from keckdrpframework.examples.pipelines.fits2png_pipeline import Fits2pngPipeline
+
+sys.path.extend(("../examples", ".."))
+
+def test_run_example():
+    f = Framework(Fits2pngPipeline, "../examples/config.cfg")
+    assert f is not None, "Could not create framework"
+    
+    f.ingest_data("test_files", ())
+    f.start (qm_only=False,  ingest_data_only=False, wait_for_event=False, continuous=False)
+    
+    flist = glob.glob("output/*.png")
+    assert len(flist) == 6, "Unexpected number of files"
+    
+    
