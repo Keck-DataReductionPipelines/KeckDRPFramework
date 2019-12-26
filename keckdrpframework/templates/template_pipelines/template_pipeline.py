548452ffa30050d6ea35619e1f2f3939a5a205cc
Shui H. Kwok <shkw0k@users.noreply.github.com>
2019-12-26 13:15:28 -1000

    Added context as parameter

--- keckdrpframework/templates/template_pipelines/template_pipeline.py
+++ keckdrpframework/templates/template_pipelines/template_pipeline.py
@@ -13,11 +13,11 @@ class TemplatePipeline(BasePipeline):
 
     event_table = {"next_file": ("simple_fits_reader", "file_ready", "file_ready"), "file_ready": ("template_action", None, None)}
 
-    def __init__(self):
+    def __init__(self, context):
         """
         Constructor
         """
-        BasePipeline.__init__(self)
+        BasePipeline.__init__(self, context)
 
     def template_action(self, action, context):
         print("Template action", action)
