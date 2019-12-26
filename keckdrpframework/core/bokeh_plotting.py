5d549c62fd865864015106ca9931929ad2fc190b
Shui H. Kwok <shkw0k@users.noreply.github.com>
2019-12-26 12:24:05 -1000

    Removed

--- keckdrpframework/core/bokeh_plotting.py
+++ keckdrpframework/core/bokeh_plotting.py
@@ -1,19 +1,21 @@
-'''
+"""
 Created on Aug 8, 2019
 
 @author: lrizzi
-'''
+"""
 
 from bokeh.io import curdoc
 from bokeh.plotting.figure import Figure
 from bokeh.models import Column
+
 
 def bokeh_plot(plot):
 
     p = curdoc().select_one(selector=dict(type=Figure))
     c = curdoc().select_one(selector=dict(type=Column))
     c.children.remove(p)
-    c.children.insert(0,plot)
+    c.children.insert(0, plot)
+
 
 def bokeh_add_plot(glyph):
 
