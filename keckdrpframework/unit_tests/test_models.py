adb7b2efe67ac9c14f53217c67e5972014559ab6
Dave Rumph <drumph@Daves-MacBook-Pro-2.local>
2020-03-30 16:23:24 -0700

    Added new test It's pretty comprehensive, testing most if not all of the functionality.

--- keckdrpframework/unit_tests/test_models.py
+++ keckdrpframework/unit_tests/test_models.py
@@ -38,6 +38,55 @@ def test_arguments_2():
 
     assert s == "name: test, a: 1, b: 2, c: 3", "Arguments str() failed"
 
+def test_arguments_3():
+    args = Arguments(4, 7, 19, name="test", a=1, c="3")
+    assert args[0] == 4 and args[2] == 19 and args['a'] == 1, "Arguments: constructor failed"
+
+    assert len(args) == 3, f"Arguments: len returns wrong value ({len(args)}, should be 3)"
+    args_iter = iter(args)
+    for ix in range(len(args)):
+        assert next(args_iter) == args[ix], "Arguments: Iteration doesn't match index"
+    try:
+        val = next(args_iter)
+    except StopIteration:
+        pass
+    except Exception:
+        assert False, "Arguments: unexpected exception on iteration"
+    else:
+        assert False, "Arguments: Expected StopIteration exception not raised"
+    
+    assert args.len_kw() == 3, f"Arguments: len_kw returns wrong value ({args.len_kw()}, should be 3"
+    args_iter_kw = args.iter_kw()
+    val = next(args_iter_kw)
+    assert val == "name" and args[val] == "test", "Arguments: keyword iteration does not match"
+    val = next(args_iter_kw)
+    assert val == "a" and args[val] == 1, "Arguments: keyword iteration does not match (2)"
+    val = next(args_iter_kw)
+    assert val == "c" and args[val] == "3", "Arguments: keyword iteration does not match (3)"
+    try:
+        val = next(args_iter_kw)
+    except StopIteration:
+        pass
+    except Exception:
+        assert False, "Arguments, unexpected exception"
+    else:
+        assert False, "Arguments: Expected StopIteration exception not raised"
+
+    args.insert(0, 1)
+    assert len(args) == 4 and args[0] == 1 and args[2] == 7, "Arguments: insert failed"
+
+    val = args.pop()
+    assert len(args) == 3 and val == 19, "Arguments: pop failed"
+
+    args.append(24)
+    assert len(args) == 4 and args[3] == 24, "Arguments: append failed"
+
+    args.extend(5, 6, 7)
+    assert len(args) == 7 and args[4] == 5 and args[6] == 7, "Arguments: extend failed"
+
+    args.update(d= "D", e="E")
+    assert args.len_kw() == 5 and args.d == "D", "Arguments: update failed"
+
 
 #
 # Processing context tests
